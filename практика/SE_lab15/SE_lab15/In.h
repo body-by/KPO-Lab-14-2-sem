a519f379c08be03bd6e0da6213b0a0bbf26e4d69
body-by <151786170+body-by@users.noreply.github.com>
2024-06-25 23:39:07 +0300

    erwfafasf

---
+++ практика/SE_lab15/SE_lab15/In.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+#define IN_MAX_LEN_TEXT 1024*1024 // максимальный размер исходного кода
+#define IN_CODE_ENDL '\n' // символ конца строки
+
+// таблица проверки входной информации, индекс = код (Windows-1251) символа
+// значения IN::F - запрещенный символ, IN::F = разрешенный символ, IN::F - игнорировать (не вводить),
+// если 0 <= значение < 256 - то вводится данное значение
+
+#define MAX_LEN_LINE 1000
+namespace In
+{
+	void dell_in(char* str, int index);
+	struct IN
+	{
+		enum
+		{
+			T = 1024, // допустимый символ
+			F = 2048, // недопустимый
+			I = 4096 // игнорировать, иначе изменить
+		};
+
+		int size, // размер исходного кода
+			lines, // количество строк
+			ignore; // количество проигнорированных символов
+
+		unsigned char* text; // исходный код(Windows-1251);
+
+		int code[256] =
+		{
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 0
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 16
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 32
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 48
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 64
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 80
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 96
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 112
+
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 128
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 144
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 160
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 176
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 192
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 208
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, // 224
+			IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F, IN::F
+		}; // таблица проверки: T, F, I новое значение
+
+		IN()
+		{ //   пробел     0          2          4         G          L           U          S          H          E          N         Y           A
+			code[32] = code[48] = code[50] = code[52] = code[71] = code[76] = code[85] = code[83] = code[72] = code[69] = code[78] = code[89] = code[65] =
+				//     P         0          L           I       N         A            g          l           u           s          h           e         n               y         a
+				code[80] = code[79] = code[76] = code[73] = code[78] = code[65] = code[103] = code[108] = code[117] = code[115] = code[104] = code[99] = code[110] = code[121] = code[97] = 
+				//      p          o         l         i           n             a        Г          Л          У               Ш            Е         Н            Я
+				code[112] = code[111] = code[108] = code[105] = code[110] = code[97] = code[195] = code[203] = code[211] = code[216] = code[197] = code[205] = code[223] =
+				//     П            О         Л           И          Н            А         г          л           у           ш           е          н             я
+				code[207] = code[206] = code[203] = code[201] = code[205] = code[192] = code[227] = code[235] = code[243] = code[248] = code[229] = code[237] = code[255] =
+				//     п          о         л          и             н           а            -
+				code[239] = code[238] = code[235] = code[232] = code[237] = code[224] = code[45] = IN::T;
+
+			code[192] = '-';
+			code[88] = IN::I;
+			
+		}
+	};
+
+	IN getin(wchar_t infile[]);
+}
